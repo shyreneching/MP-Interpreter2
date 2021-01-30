159340cd1b8044195b44c0b9e117b6a8f42fd1e2
ErikaChan <43471017+ErikaChan@users.noreply.github.com>
2021-01-30 20:46:51 +0800

    UI running

--- MP Interpreter/src/model/ErrorListener.java
+++ MP Interpreter/src/model/ErrorListener.java
@@ -139,6 +139,10 @@ public class ErrorListener extends BaseErrorListener {
     public String toString() {
         return errorMsg;
     }
+
+    public void resetErrors(){
+        this.errorMsg = "";
+    }
 //    @Override public void reportAmbiguity(org.antlr.v4.runtime.Parser recognizer, org.antlr.v4.runtime.dfa.DFA dfa, int startIndex, int stopIndex, boolean exact, java.util.BitSet ambigAlts, org.antlr.v4.runtime.atn.ATNConfigSet configs) {
 //        System.err.println(ambigAlts);
 //        System.err.println(configs);
